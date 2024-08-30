96224e9b7d5af7958e3b4e61b10d3eae342459e7
David-Hakobyan1 <84774130+David-Hakobyan1@users.noreply.github.com>
2024-08-30 17:48:53 +0400

    Update hello.py
    Updated hello.py file

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello")
+"""
+hello.py
+
+This module demonstrates a simple 'Hello, World!' function.
+"""
+
+def say_hello():
+    """ Prints 'Hello, World!' to the console."""
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    say_hello()
